ece785ef9a3c97444256ec453242d68e4187daa8
Waqas Ahmed <waqas@opensource.com.pk>
2016-07-27 18:37:46 +0500

    Update opengraph.py

--- opengraph/opengraph.py
+++ opengraph/opengraph.py
@@ -26,10 +26,6 @@ class OpenGraph(dict):
             'attr': 'property',
             'required_attrs': ['player']
         }),
-        ('twitter', {
-            'attr': 'name',
-            'required_attrs': ['player']
-        }),
         ('og', {
             'attr': 'property',
             'required_attrs': ['title', 'type', 'image', 'url']
